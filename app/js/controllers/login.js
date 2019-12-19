7de6d9bb21cc1b42596af2fb2925e58bc9e8a1a5
cj-Pacific-Blue-IT <57339692+cj-Pacific-Blue-IT@users.noreply.github.com>
2019-12-19 14:37:15 +0800

    Login-Logout

--- app/js/controllers/login.js
+++ app/js/controllers/login.js
@@ -2,22 +2,68 @@
 
 /* Controllers */
   // signin controller
-app.controller('LoginFormController', ['$scope', '$http', '$state', function($scope, $http, $state) {
-    $scope.user = {};
-    $scope.authError = null;
+app.controller('LoginFormController', ['$firebaseAuth', '$firebaseArray', '$scope', '$http', '$state', function($firebaseAuth, $firebaseArray, $scope,  $http, $state, ) {
+    
     $scope.login = function() {
-      $scope.authError = null;
+      
+      var username = $scope.user.email;
+      var password = $scope.user.password;
+      var auth = $firebaseAuth();
       // Try to login
-      $http.post('data/login.json', {email: $scope.user.email, password: $scope.user.password})
-      .then(function(response) {
-        if ( !response.data.user ) {
-          $scope.authError = 'Email or Password not right';
-        }else{
-          $state.go('app.dashboard');
-        }
-      }, function(x) {
-        $scope.authError = 'Server Error';
+      auth.$signInWithEmailAndPassword(username, password)
+      .then(function() {
+        $state.go('app.ui.googlemapfull');
+        $scope.authError = false;
+        console.log('User Login Succesful');
+      }) .catch(function(error){
+        $scope.authError = true;
+        $scope.authError = error.message;
       });
     };
+
+    $scope.login2 = function() {
+
+      var ref = firebase.database().ref("users");
+          console.log("Signing in");
+          var provider = new firebase.auth.GoogleAuthProvider();
+          firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+
+        var userDelete = firebase.auth().currentUser;
+
+        ref.orderByChild("email").equalTo(user.email).once("value")
+      .then (function(snapshot) {
+
+        //Check if email is already exist in real time database
+        var exist = snapshot.exists();
+        if (!exist){
+          console.log("Don't Exists!");
+          console.log("There is no user record corresponding to this identifier. The user may have been deleted.");
+          $scope.authError = 'There is no user record corresponding to this identifier. The user may have been deleted.';
+          user.delete().then(function() {
+            // User deleted.
+            console.log('Successfully deleted user');
+          }).catch(function(error) {
+            // An error happened.
+            console.log('Error deleting user:', error);
+          });
+        } else {
+          app(user);
+          console.log("Signed in");
+        }
+      });
+      }) .catch(function(error){
+        $scope.authError = true;
+        $scope.authError = error.message;
+      });
+    };
+
+    function app(user){
+      $state.go('app.ui.googlemapfull');
+    }
   }])
 ;
